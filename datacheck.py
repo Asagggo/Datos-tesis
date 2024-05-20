b6d2add0dbb62e067073b448f8ab40a7fe8cc694
Asagggo <adriansangonz@outlook.es>
2024-05-19 23:06:55 -0600

    Script funcionando para generar tabla en excel con dataframe extraida de world data bank, recuerda cambiar 'IND' dentro de la funcion dataframe a 'CAN' 'CH' 'USA' para cambiar de país, así como cambiar el nombre del archivo excel

--- datacheck.py
+++ datacheck.py
@@ -1,3 +1,6 @@
 import wbgapi as wb
-info= wb.data.DataFrame(['SL.UEM.TOTL.ZS', 'FP.CPI.TOTL.ZG', 'SP.POP.GROW', 'GC.XPN.TOTL.GD.ZS', 'BX.KLT.DINV.WD.GD.ZS'], 'USA', mrv=10) # most recent 5 years of inflation data for the USA
+info= wb.data.DataFrame(['SL.UEM.TOTL.ZS', 'FP.CPI.TOTL.ZG', 'SP.POP.GROW', 'GC.XPN.TOTL.GD.ZS', 'BX.KLT.DINV.WD.GD.ZS'], 'IND', mrv=14, skipBlanks=True, columns='series') # most recent 15 years of inflation data for the USA, CHINA E INDIA
 print(info)
+# Guardar el dataframe en un archivo Excel
+info.to_excel(r'C:\Users\adria\Downloads\Data modelo tesis\Anual\Estados Unidos\IND.xlsx', index=False)
+# Guardar el dataframe en un archivo Excel
