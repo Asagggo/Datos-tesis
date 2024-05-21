1fce5950881bd0461adf6763b5f788e84a139e9a
Asagggo <adriansangonz@outlook.es>
2024-05-20 20:33:13 -0600

    Codigo final funcionando para extraer base de datos en conjunto de variables del world data bank en conjunto de países y convertirlo a excel

--- datacheck.py
+++ datacheck.py
@@ -1,6 +1,6 @@
 import wbgapi as wb
-info= wb.data.DataFrame(['SL.UEM.TOTL.ZS', 'FP.CPI.TOTL.ZG', 'SP.POP.GROW', 'GC.XPN.TOTL.GD.ZS', 'BX.KLT.DINV.WD.GD.ZS'], 'IND', mrv=14, skipBlanks=True, columns='series') # most recent 15 years of inflation data for the USA, CHINA E INDIA
+info= wb.data.DataFrame(['SL.UEM.TOTL.ZS', 'FP.CPI.TOTL.ZG', 'SP.POP.GROW', 'GC.XPN.TOTL.GD.ZS', 'BX.KLT.DINV.WD.GD.ZS'], ['USA','GBR','ISR','CAN','FRA','IND','JPN','DEU','SGP','KOR'], range(2000,2023), skipBlanks=True, columns='series') # most recent 15 years of inflation data for the USA, CHINA E INDIA
 print(info)
+#Agregar columna de años
 # Guardar el dataframe en un archivo Excel
-info.to_excel(r'C:\Users\adria\Downloads\Data modelo tesis\Anual\Estados Unidos\IND.xlsx', index=False)
-# Guardar el dataframe en un archivo Excel
+info.to_excel(r'C:\Users\adria\Downloads\Data modelo tesis\Anual\DATAFRAMEVARIABLESWORLDBANKDATA.xlsx', index=True)
