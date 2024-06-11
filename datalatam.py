0906230d858f4449ca776bc68f274cf63127b5f6
Asagggo <adriansangonz@outlook.es>
2024-06-11 01:04:47 -0600

    Se agranda grupo de países de latam a 14

--- datalatam.py
+++ datalatam.py
@@ -1,6 +1,6 @@
 import wbgapi as wb
-info= wb.data.DataFrame(['SL.UEM.TOTL.ZS', 'FP.CPI.TOTL.ZG', 'SP.POP.GROW', 'NE.CON.GOVT.ZS', 'BX.KLT.DINV.WD.GD.ZS'], ['CHL','COL','CRI','MEX','BRA'], range(2000,2023), skipBlanks=True, columns='series') # most recent 15 years of inflation data for the USA, CHINA E INDIA
+info= wb.data.DataFrame(['SL.UEM.TOTL.ZS', 'FP.CPI.TOTL.ZG', 'SP.POP.GROW', 'NE.CON.GOVT.ZS', 'BX.KLT.DINV.WD.GD.ZS'], ['BOL', 'BRA', 'CHL', 'COL', 'CRI', 'DOM', 'ECU', 'SLV', 'GTM', 'HND', 'MEX', 'NIC', 'PRY', 'PER'], range(2000,2023), skipBlanks=True, columns='series') # most recent 15 years of inflation data for the USA, CHINA E INDIA
 print(info)
 #Agregar columna de años
 # Guardar el dataframe en un archivo Excel
-info.to_excel(r'C:\Users\adria\Downloads\Data modelo tesis\Anual\WorldDataBankLatam(expense general).xlsx', index=True)
+info.to_excel(r'C:\Users\adria\Downloads\Data modelo tesis\Anual\WorldDataBankLatam14(expense general).xlsx', index=True)
